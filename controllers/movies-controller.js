5c7c42e3fed5d5b1620c9bedad7e81616e9400b2
debbie chong <giyoungchong1980@gmail.com>
2018-06-02 21:29:04 -0400

    Add movie model and render view

--- controllers/movies-controller.js
+++ controllers/movies-controller.js
@@ -1,5 +1,23 @@
-module.exports = {
+/*module.exports = {
   index: (req,res) => {
     res.render('movies/index');
   }
 };
+*/
+
+const Movie = require('../models/movies');
+const moviesController = {};
+
+moviesController.index = (req,res) => {
+  Movie.findAll()
+    .then(movies => {
+      res.render('movies/index', {
+        movies: movies
+      });
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = moviesController;
